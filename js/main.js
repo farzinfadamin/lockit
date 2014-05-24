514d6a7789c0e49aa5d6ed19a4df7523ae1d68c4
Farzin Fadamin <ffadamin@gmail.com>
2014-05-25 02:30:15 +0430

    Update lockit forms

--- js/main.js
+++ js/main.js
@@ -1,3 +1,118 @@
+var lockit = function(){
+
+  var 
+    $window,
+    $thankyou,
+    $modalBox;
+
+  var mandrilInstance;
+  
+  return {
+
+    init: function(){
+
+      $window = $(window);
+      
+      $modalBox = $('.modal-box');
+
+      mandrilInstance = new mandrill.Mandrill('5Zcfo9PttFcLZ78BLQPFZQ');
+      
+      $thankyou = $('.thankyou'),
+
+      $window.on('scroll.lazyload', $.proxy(this.onScroll, this) );
+
+      $window.on('scroll', function(){
+        $window.trigger('scroll.lazyload');
+      });
+
+      
+      $('.notify-form').on('submit', function(e){ 
+        e.preventDefault(); 
+      });
+
+      $('.notify-form button').on('click', $.proxy(this.onNotifySubmit, this));
+
+    },
+
+    onScroll: function(){
+      
+      var scrollTop = $window.scrollTop(),
+          $sections = $('[class ^=section-]:not(.active');
+
+      if( ! $sections.length){
+        $window.off('scroll.lazyload');
+      }
+
+
+      $sections.each(function(){
+        if ( scrollTop > $(this).offset().top  - $(this).height() ) { 
+          $(this).addClass('active');
+        }
+      });
+
+    },
+
+
+    onNotifySubmit: function(e){
+
+      var from = $.trim($('.get-email :text').val());
+      
+      this.submitEmail( from );      
+    
+    },
+
+    submitEmail: function(from){
+
+      var to = 'mahdi.pedram@mexo.co';
+
+      if(! this.validateEmail(from) ){
+        console.log('invalid');
+        this.invalidEmail();
+        return;
+      }
+
+      // create a variable for the API call parameters
+      var params = {
+          message: {
+              from_email: from,
+              to: [{
+                email: to
+              }],
+              subject: "LockIt User Subscription",
+              text: " has sunscribed to LockIT"
+          }
+      };
+
+      mandrilInstance.messages.send(params, $.proxy(function(res) {
+      
+          this.emailSent();
+      
+      }, this), $.proxy(function(err) {
+
+          this.invalidEmail();
+      
+      }, this));
+
+    },
+
+    validateEmail: function(email) { 
+      var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
+    },
+
+    invalidEmail: function(){
+      $('.notify-form').addClass('error');
+    },
+
+    emailSent: function(){
+      $modalBox.removeClass('active')
+      $thankyou.addClass('active');
+    } 
+  }
+
+}();
+
+
 $(document).ready(function(){
 
 	$('.lk-handiphone').addClass('show');
@@ -22,6 +137,8 @@ $(document).ready(function(){
   $closeBtn.on('click', function(){
     $modalBox.removeClass('show');
     $modalBox.children().removeClass('active');
-  })
+  });
 
-})
+  lockit.init();
+
+});
